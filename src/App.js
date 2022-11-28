bd90a5406f271ed0410faeecd84fd52bda3fc9f8
Daniel <daniel.afilipoaie@gmail.com>
2022-11-28 22:23:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,47 @@ import "./App.css";
 import Content from "./Content";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, checked: false, item: "One half pound if Cocoa Covered" },
+    { id: 2, checked: false, item: "Item2" },
+    { id: 3, checked: false, item: "Item3" },
+  ]);
   // const randomNames = ["daniel", "johny", "Klaus", "Other"];
   // let q;
   // const name = function () {
   //   q = Math.floor(Math.random() * randomNames.length);
   //   return randomNames[q];
 
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id
+        ? {
+            ...item,
+            checked: !item.checked,
+          }
+        : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shopinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shopinglist", JSON.stringify(listItems));
+  };
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Groceries List" />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,43 +1,19 @@
 import React from "react";
-import { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    { id: 1, checked: false, item: "One half pound if Cocoa Covered" },
-    { id: 2, checked: false, item: "Item2" },
-    { id: 3, checked: false, item: "Item3" },
-  ]);
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id
-        ? {
-            ...item,
-            checked: !item.checked,
-          }
-        : item
-    );
-    setItems(listItems);
-    localStorage.setItem("shopinglist", JSON.stringify(listItems));
-  };
+import ItemList from "./ItemList";
 
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            />
-            <label onDoubleClick={() => handleCheck(item.id)}>
-              {item.item}
-            </label>
-            <FaTrashAlt role="button" tabIndex={0} />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty!</p>
+      )}
     </main>
   );
 };
